e0d6063b984c2204b161756550ba86791c923eda
rbetha <rbetha@developerxd095.ihtech.com>
2016-11-29 12:23:54 +0530

    commieted second time

--- ProjectGit/src/Test/TestSample1.java
+++ ProjectGit/src/Test/TestSample1.java
@@ -3,7 +3,7 @@ package Test;
 public class TestSample1 {
 	
 	public static void main(String args[]) {
-		System.out.println("Hello World1....");
+		System.out.println("Hello World2....");
 			
 
 }}
