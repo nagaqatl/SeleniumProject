73ddc7d2e100be7ae78822a2e43ee1edfd90d0bc
ulanka <ulanka@developerxd087.ihtech.com>
2016-11-30 12:50:32 +0530

    uday commited

--- ProjectGit2/src/Test2/TestSampleGit2.java
+++ ProjectGit2/src/Test2/TestSampleGit2.java
@@ -2,7 +2,7 @@ package Test2;
 
 public class TestSampleGit2 {
 	public static void main(String args[]){
-		System.out.println("This is SampleGit2 World...");
+		System.out.println("This is SampleGit2 World8888...");
 	}
 
 }
